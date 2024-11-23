8815806612b6e2fd6bb95d7099189edfc6efce2a
pfgq <pfgq@qq.com>
2024-11-23 15:01:03 +0800

    Update process.c

--- kernel/process.c
+++ kernel/process.c
@@ -4,6 +4,8 @@
 #include <linux/tty.h>
 #include <linux/mm.h>
 #include <linux/version.h>
+#include <linux/types.h>  
+#include <linux/uidgid.h> 
 
 #define ARC_PATH_MAX 256
 
@@ -15,50 +17,49 @@ extern void mmput(struct mm_struct *);
 
 uintptr_t get_module_base(pid_t pid, char *name)
 {
-	struct pid *pid_struct;
-	struct task_struct *task;
-	struct mm_struct *mm;
-	struct vm_area_struct *vma;
+    struct pid *pid_struct;
+    struct task_struct *task;
+    struct mm_struct *mm;
+    struct vm_area_struct *vma;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
-	struct vma_iterator vmi;
+    struct vma_iterator vmi;
 #endif
 
-	pid_struct = find_get_pid(pid);
-	if (!pid_struct)
-	{
-		return false;
-	}
-	task = get_pid_task(pid_struct, PIDTYPE_PID);
-	if (!task)
-	{
-		return false;
-	}
-	mm = get_task_mm(task);
-	if (!mm)
-	{
-		return false;
-	}
-	mmput(mm);
+    pid_struct = find_get_pid(pid);
+    if (!pid_struct)
+    {
+        return 0; 
+    }
+    task = get_pid_task(pid_struct, PIDTYPE_PID);
+    if (!task)
+    {
+        return 0;
+    }
+    mm = get_task_mm(task);
+    if (!mm)
+    {
+        return 0;
+    }
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
-	vma_iter_init(&vmi, mm, 0);
-	for_each_vma(vmi, vma)
+    vma_iter_init(&vmi, mm, 0);
+    for_each_vma(vmi, vma)
 #else
-	for (vma = mm->mmap; vma; vma = vma->vm_next)
+    for (vma = mm->mmap; vma; vma = vma->vm_next)
 #endif
-	{
-		char buf[ARC_PATH_MAX];
-		char *path_nm = "";
+    {
+        char buf[ARC_PATH_MAX];
+        char *path_nm = "";
 
-		if (vma->vm_file)
-		{
-			path_nm =
-				file_path(vma->vm_file, buf, ARC_PATH_MAX - 1);
-			if (!strcmp(kbasename(path_nm), name))
-			{
-				return vma->vm_start;
-			}
-		}
-	}
-	return 0;
+        if (vma->vm_file)
+        {
+            path_nm =
+                file_path(vma->vm_file, buf, ARC_PATH_MAX - 1);
+            if (!strcmp(kbasename(path_nm), name))
+            {
+                return vma->vm_start;
+            }
+        }
+    }
+    return 0;
 }
