b526e4b6289436ce0c5f15a2dded1230a3008323
pfgq <pfgq@qq.com>
2024-11-24 13:15:17 +0800

    Update process.c

--- kernel/process.c
+++ kernel/process.c
@@ -9,10 +9,6 @@
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
-extern void mmput(struct mm_struct *);
-#endif
-
 uintptr_t get_module_base(pid_t pid, char *name)
 {
     struct pid *pid_struct;
@@ -44,8 +40,15 @@ uintptr_t get_module_base(pid_t pid, char *name)
         return 0;  // 返回 0，表示获取 mm_struct 失败
     }
 
-    // 清理 mm_struct
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
+    // 适用于 5.4.61 及以上版本
     mmput(mm);
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0))
+    // 在 4.19 版本上使用 exit_mmap
+    down_write(&mm->mmap_sem); // 锁定 mmap
+    exit_mmap(mm);
+    up_write(&mm->mmap_sem);   // 解锁 mmap
+#endif
 
     // 遍历进程的虚拟内存区域
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0))
@@ -70,7 +73,7 @@ uintptr_t get_module_base(pid_t pid, char *name)
             }
         }
     }
-    
+
     // 如果没有找到匹配的模块，返回 0
     return 0;
 }
